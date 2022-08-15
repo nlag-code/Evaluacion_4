7b32e9aa9b359b327241421a373599dfd2c9150c
ncace <ncaceres1995@gmail.com>
2022-08-15 03:34:09 -0400

    version_3: Con comentarios

--- main.py
+++ main.py
@@ -3,22 +3,26 @@ import requests
 from netmiko import ConnectHandler
 import time
 
+#Variables que utilizaremos para hacer la consulta utilizando el protocolo restconf.
 url = "https://"
 auth = (conf.user, conf.pwd)
 headers = {'Accept': 'application/yang-data+json'}
 
 
+#Funcion con la que importaremos listado de ip que necesitamos realizar el respaldo
 def leer_archivo():
     f = open('hosts.txt', 'r')
     lista = list(f)
     f.close()
     return lista
 
+#Funcion con la que exportaremos cada respaldo realizado
 def crear_respaldo():
     backup = open(hostname + '.txt', 'w')
     backup.write(netmiko_test())
     backup.close()
 
+#Funcion donde utilizaremos funciones gracias a la libreria netmiko, esta nos permitira conectarnos por ssh a los equipos y lanzar comandos de IOS Cisco.
 def netmiko_test():
     credenciales = {
         'device_type': 'cisco_ios',
@@ -37,13 +41,13 @@ def netmiko_test():
 requests.packages.urllib3.disable_warnings()
 
 
-for i in leer_archivo():
-    host = i.rstrip('\n')
-    salida = requests.get(url + host +"/restconf/data/Cisco-IOS-XE-native:native/", verify=False, headers=headers, auth=auth)
-    hostname = salida.json()["Cisco-IOS-XE-native:native"]["hostname"]
-    netmiko_test()
-    crear_respaldo()
-    time.sleep(1)
-    print("Respaldo de equipo "+ hostname + " realizado de forma exitosa")
+for i in leer_archivo(): # Con este bucle leeremos el archivo hosts donde estan las ip
+    host = i.rstrip('\n') # variable donde guardaremos cada ip, le quitamos el salto de linea.
+    salida = requests.get(url + host +"/restconf/data/Cisco-IOS-XE-native:native/", verify=False, headers=headers, auth=auth) # hacemos una consulta get gracias al protocolo restconf conf en los equipos cisco
+    hostname = salida.json()["Cisco-IOS-XE-native:native"]["hostname"] #a la respuesta le filtramos el hostname que es lo que nos interesa
+    netmiko_test() #llamamos a la funcion netmiko para conectarnos al host y lanzar el comando show running config
+    crear_respaldo() # llamamos a la funcion crear respaldo donde creamos un archivo con el nombre del host y guardamos el show run.
+    time.sleep(1) # generamos un tiempo de espera de 1 segundo para que se vea lindo.
+    print("Respaldo de equipo "+ hostname + " realizado de forma exitosa") # Informamos por un mensaje de consola que la tarea fue realizada de forma exitosa.
 
 
